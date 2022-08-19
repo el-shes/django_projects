0bac011b479b56c51b90effdbf6cf1bc8fcb65c7
rrfpCSIR <shes980@gmail.com>
2022-08-19 23:15:15 +0300

    adding social login with GitHub

--- django_projects/settings.py
+++ django_projects/settings.py
@@ -162,7 +162,7 @@ try:
     SOCIAL_AUTH_GITHUB_KEY = github_settings.SOCIAL_AUTH_GITHUB_KEY
     SOCIAL_AUTH_GITHUB_SECRET = github_settings.SOCIAL_AUTH_GITHUB_SECRET
 except:
-    print('When you want to use social login, please see dj4e-samples/github_settings-dist.py')
+    print('When you want to use social login, please see dj4e-samples/github_settings.py')
 
 # https://python-social-auth.readthedocs.io/en/latest/configuration/django.html#authentication-backends
 # https://simpleisbetterthancomplex.com/tutorial/2016/10/24/how-to-add-social-login-to-django.html
